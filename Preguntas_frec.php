0e7970943a92fe42ec952e36eee161ffb3066812
Xoch-CV <54358593+Xoch-CV@users.noreply.github.com>
2019-09-26 21:16:09 -0300

    Add files via upload
    FAQ y Contacto con navbar como en index

--- Preguntas_frec.php
+++ Preguntas_frec.php
@@ -1,3 +1,21 @@
+<?php
+session_start();
+include('functions.php');
+is_remembered();
+
+$claseperfil='d-none';
+$claselogout='';
+$perfil='';
+$foto='files/Avatar.jpg';
+
+if(isset($_SESSION['user'])){
+  $claseperfil='';
+  $claselogout='d-none';
+  $perfil=$_SESSION['user']['nombre'];
+  $foto=$_SESSION['user']['foto'];
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,24 +23,64 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" type="text/css" href="Semantic_ui/project/semantic/dist/semantic.min.css">
-    <link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="css/styles.css">
-    <script
-      src="https://code.jquery.com/jquery-3.1.1.min.js"
-      integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
-      crossorigin="anonymous">
-    </script>
-    <script src="Semantic_ui/project/semantic/dist/semantic.min.js"></script>
-
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/styleform.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link href="fontawesome/css/all.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,600,700,900&display=swap" rel="stylesheet">
     <title>Preguntas Frecuentes</title>
 </head>
 <body>
-    <div class="ui grid">
+  <nav class="navbar navbar-expand-md navbar-dark">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target=".dual-collapse2">
+      <i class="fas fa-bars"></i>
+    </button>
+    <div class="navbar-collapse collapse w-100 order-1 order-md-0 dual-collapse2">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="index.php">Inicio</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="Preguntas_frec.php">f.a.q.</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="Contacto.php">Contacto</a>
+        </li>
+      </ul>
+    </div>
+    <div class="order-0">
+      <h1>T<span class="iso">!</span>CKET</h1>
+    </div>
+    <div class="navbar-collapse collapse w-100 order-3 dual-collapse2">
+      <ul class="navbar-nav ml-auto">
+        <div id="login" class="<?=$claseperfil?>">
+        <li class="nav-item2 dropdown">
+          <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Hola <?=$perfil?>!</a>
+          <div class="dropdown-menu">
+            <a class="dropdown-item" href="Perfil.php">Ir a mi perfil</a>
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item" href="Logout.php">Cerrar sesión</a>
+          </div>
+        </li>
+        <li class="nav-link"><img src="<?=$foto?>" alt="avatar"></li>
+        </div>
+        <div id="login" class="<?=$claselogout?>">
+        <li class="nav-item">
+          <a class="nav-link" href="Loginform.php">Log in</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="Registro.php">sign up</a>
+        </li>
+      </div>
+      </ul>
+    </div>
+  </nav>
+    <div class="ui center aligned grid">
         <div class="column">
             <div class="ui segment">
-                <h2>¿Tienes dudas?</h2>
+                <h4 id="sem">¿Tienes dudas?</h4>
                     <div class="ui secondary segment">
-                        <h2>Selecciona un asunto</h2>
+                        <h4 id="sem">Selecciona un asunto</h4>
                         <div class="ui vertical segment">
                             <i class="money bill alternate outline icon"></i>
                             <a id="asunto" href="">Compra Ticket</a>
@@ -35,7 +93,8 @@
                             <i class="cart arrow down icon"></i>
                             <a id="asunto" href="">Productos</a>
                         </div>
-                        <h2>Las preguntas más frecuentes</h2>
+                        <br>
+                        <h4 id="sem">Las preguntas más frecuentes</h4>
                         <div class="ui list">
                                 <a class="item">¿Cómo compro boletos?</a>
                                 <a class="item">¿Cómo puedo pagar los boletos?</a>
@@ -46,22 +105,16 @@
                         </div>
                     </div>
             </div>
-        </div> 
-    </div> 
-    <!--
-    <div class="ui grid">
-            <div class=" eight wide column">
-                    <div class="ui form">
-                         <div class="field">
-                                <div class="ui segment">
-                                        <label>Usuario</label>
-                                        <input type="email" placeholder="joe@schmoe.com">
-                                </div>
-                         </div>
-                    </div>
-            </div>
         </div>
-    
-    -->
+    </div>
+    <script
+      src="https://code.jquery.com/jquery-3.1.1.min.js"
+      integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
+      crossorigin="anonymous">
+    </script>
+    <script src="Semantic_ui/project/semantic/dist/semantic.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </body>
 </html>
